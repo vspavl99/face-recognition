ea481a249801d8795b102b23aef194c082192dd1
vpavlishen <v.pavlishen@businessdecision.ru>
2023-08-19 11:41:48 +0300

    initial commit

---
+++ src/data/process_raw_data.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+import zipfile
+
+import click
+
+
+@click.command()
+@click.option('--raw_data_path', type=str, help='Path to a archived data file.')
+@click.option('--baked_data_dir', type=str, help='Path to a directory where to save prepared data')
+def main(raw_data_path: str, baked_data_dir: str):
+
+    raw_data_path = Path(raw_data_path)
+    baked_data_dir = Path(baked_data_dir)
+
+    unzip_archive(raw_data_path=raw_data_path, baked_data_dir=baked_data_dir)
+
+
+def unzip_archive(raw_data_path: Path, baked_data_dir: Path):
+    with zipfile.ZipFile(file=raw_data_path, mode='r') as zip_ref:
+        baked_data_dir.mkdir(exist_ok=True)
+        zip_ref.extractall(baked_data_dir)
+
+
+if __name__ == '__main__':
+    main()
